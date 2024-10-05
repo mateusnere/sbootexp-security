7c934279cc467978782d8ca46bd700806e555eea
Mateus Maciel Nere <mateus.macieln@gmail.com>
2024-10-04 21:18:12 -0300

    Versão do projeto com a autênticação feita com UserDetailsService.

---
+++ src/main/java/io/github/mateusnere/sbootexpsecurity/api/FooController.java
@@ -0,0 +1,25 @@
+package io.github.mateusnere.sbootexpsecurity.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicRoute() {
+        return ResponseEntity.ok("Public route ok!");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> privateRoute(Authentication authentication) {
+        return ResponseEntity.ok("Private route ok! User: " + authentication.getName());
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminRoute() {
+        return ResponseEntity.ok("Admin route ok!");
+    }
+}
